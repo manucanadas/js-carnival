7147d3a7ddf4ed1cd0e696eee5a702078232ad00
Manuel Canadas <nanocanadas@gmail.com>
2021-03-04 15:19:05 +1300

    changeHead function

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,8 @@
   // -    -   -   -   -  //
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
-
+function changeClownHead() {
+  document.getElementById("head").src="./images/head1.png"
+}
+changeClownHead()
 console.log("Dress The Clown!")
