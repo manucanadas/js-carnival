d7d98475267b159b8cbdf5d2dbcbd83ed656ad83
Manuel Canadas <nanocanadas@gmail.com>
2021-03-05 13:06:24 +1300

    whack the mole done

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -2,4 +2,32 @@
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+// get a random cell
+// mole img appears in random cell
+// click on the mole
+// mole respawns in another cell
+
+var cells = document.getElementsByTagName("TD");
+
+var randomIndex = Math.floor(Math.random() * cells.length);
+
+var randomCell = cells[randomIndex];
+
+var mole = document.createElement("img");
+
+mole.src = "./mole.PNG";
+
+mole.id = "mole";
+
+randomCell.appendChild(mole);
+
+mole.onclick = whackMole;
+
+function whackMole() {
+  randomIndex = Math.floor(Math.random() * cells.length);
+  randomCell = cells[randomIndex];
+  randomCell.appendChild(mole);
+
+  var audio = new Audio("./whack-audio.wav");
+  audio.play();
+}
